052644b10f36be67e937fd3bdb36c9dc7360bd66
Gabriel Martorelli <gabriel.martorelli@hotmail.com>
2026-04-21 09:44:15 -0300

    add controller user

---
+++ Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace minha_primeira_api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Gabriel");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
+using minha_primeira_api;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container. 
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddRouting(option => option.LowercaseUrls = true);
+
 
 var app = builder.Build();
 
